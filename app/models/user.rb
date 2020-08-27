2f0efed8bcfc368a1f5fe3eef0322f9ddb7a0171
shuntagami <shuntagami23@gmail.com>
2020-08-27 19:12:57 +0900

    新規投稿におけるバリデーションの実施

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,20 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, :birth_date, presence: true
+          
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers'
+
+  with_options presence: true, format: { with: /[ぁ-んァ-ン一-龥]/, message: 'Full-width characters' } do
+    validates :lastname
+    validates :firstname
+  end
+
+  with_options presence: true, format: { with: /[ァ-ン]/, message: 'Full-width katakana characters' } do
+    validates :lastname_kana
+    validates :firstname_kana
+  end
+
 end
