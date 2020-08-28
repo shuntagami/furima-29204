6cca454c4df7e749d83daabb28e6e8f3289c23f5
shuntagami <shuntagami23@gmail.com>
2020-08-28 13:22:52 +0900

    useモデルの単体テストコード

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname              {Faker::Name.initials(number: 2)}
     email                 {Faker::Internet.free_email}
-    password              {Faker::Internet.password(min_length: 6)}
+    password              {Faker::Internet.password(min_length: 6, mix_case: true)}
     password_confirmation {password}
     birth_date            {"1930-01-01"}
     
