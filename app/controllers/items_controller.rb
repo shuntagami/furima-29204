2bd0aa74253bb1085488844ea28c265d04f74cc4
shuntagami <shuntagami23@gmail.com>
2020-09-02 12:27:38 +0900

    商品の保存とエラ〜メッセージの表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,12 @@
 class ItemsController < ApplicationController
+  before_action :move_to_index, except: [:index]
+  def new
+    @item = Item.new
+  end
+
   def create
     @item = Item.new(item_params)
-    if @article.save
+    if @item.save
       redirect_to root_path
     else
       render :new
@@ -12,7 +17,13 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(
-      :image, :name,  :description, :category_id, :condition_id, :charge_id, :shipping_place_id, :shipping_date_id, :price
-    )
+      :image, :name, :description, :category_id, :condition_id, :charge_id, :shipping_place_id, :shipping_date_id, :price
+    ).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -9,7 +9,7 @@ class Item < ApplicationRecord
   belongs_to_active_hash :shipping_date
 
   #空の投稿を保存できないようにする
-  validates :image, :name, :discription, :price, presence: true
+  validates :image, :name, :description, :price, presence: true
 
   #ジャンルの選択が「--」の時は保存できないようにする
   validates :category_id, :condition_id, :charge_id, :shipping_place_id, :shipping_date_id, numericality: { other_than: 1 } 
