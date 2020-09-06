6dcb0df2ee6bca4d321dca18f6cc6cc839a5be6b
shuntagami <shuntagami23@gmail.com>
2020-09-06 18:16:11 +0900

    imageにバリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,7 +12,7 @@ class Item < ApplicationRecord
 
   #空の投稿を保存できないようにする
   validates :name, :description, :price, presence: true
-
+  validate :image_presence
   #ジャンルの選択が「--」の時は保存できないようにする
   validates :category_id, :condition_id, :charge_id, :shipping_place_id, :shipping_date_id, numericality: { other_than: 1 } 
 
@@ -21,5 +21,11 @@ class Item < ApplicationRecord
                       :greater_than_or_equal_to=> 300, 
                       :less_than_or_equal_to => 9_999_999
                     }
+
+  def image_presence
+    unless image.attached?
+      errors.add(:image, "can't be blank" )
+    end
+  end                  
 end
 
