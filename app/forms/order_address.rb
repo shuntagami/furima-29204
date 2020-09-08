54c2a38f0560a3999769499d392d8a008fa9570f
shuntagami <shuntagami23@gmail.com>
2020-09-08 17:35:53 +0900

    OrderとAdressの保存の記述

--- app/forms/order_address.rb
+++ app/forms/order_address.rb
@@ -7,4 +7,11 @@ class OrderAddress
     validates :phone_number, format: {with: /^[0-9]+$/, message: "is invalid. You can't use hyphen(-)."}
     validates :postal_code, :city, :banti
   end
+
+  def save
+    Order.create(user_id: user.id, item_id: item.id)
+
+    Address.create(postal_code: postal_code, :shipping_place: :shipping_place, city: city, banti: banti, building_name: building_name,
+                   phone_number: phone_number order_id: order.id)
+  end
 end
