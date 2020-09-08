35fbff990d906efb0271ec028247e922450a45f1
shuntagami <shuntagami23@gmail.com>
2020-09-08 17:25:21 +0900

    バリデーションの設定

--- app/forms/order_address.rb
+++ app/forms/order_address.rb
@@ -2,4 +2,9 @@ class OrderAddress
 
   include ActiveModel::Model
   attr_accessor :postal_code, :shipping_place, :city, :banti, :building_name, :phone_number
+  with_options presence: true do
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :phone_number, format: {with: /^[0-9]+$/, message: "is invalid. You can't use hyphen(-)."}
+    validates :postal_code, :city, :banti
+  end
 end
