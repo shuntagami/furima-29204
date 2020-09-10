be53d427a93f9cb45f1ca78f45b081e3255a680a
shuntagami <shuntagami23@gmail.com>
2020-09-10 17:12:03 +0900

    soldoutの表示

--- app/forms/order_address.rb
+++ app/forms/order_address.rb
@@ -1,9 +1,9 @@
 class OrderAddress
   include ActiveModel::Model
   attr_accessor :user_id, :item_id, :order_id, :postal_code, :shipping_place_id, :city, :banti, :building_name, :phone_number, :token
-
+  validates :token, presence: true
   with_options presence: true do
-    validates :city, :banti, :token
+    validates :city, :banti
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :phone_number, format: {with: /\A[0-9]+\z/, message: "is invalid. You can't use hyphen(-)"}
     validates :shipping_place_id, numericality: { other_than: 0, message: "can't be blank" }
