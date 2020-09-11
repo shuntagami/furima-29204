40cb35b1fdd17d2895205613243729202d0f7626
shuntagami <shuntagami23@gmail.com>
2020-09-11 13:10:58 +0900

    oreder_addressモデルの単体テストコード

--- spec/factories/order_addresses.rb
+++ spec/factories/order_addresses.rb
@@ -2,9 +2,10 @@ FactoryBot.define do
   Faker::Config.locale = :ja
   factory :order_address do
     postal_code     { Faker::Address.postcode }
-    prefecture      { Faker::Address.state }
+    prefecture      { id = 2 }
     city            { Faker::Address.city }
     banti           { Faker::Address.building_number }
-    phone_number    { Faker::PhoneNumber.cell_phone }
+    phone_number    { Faker::PhoneNumber.cell_phone.delete("-") }
+    token           { "tok_0000000000000000000000000000" }
   end
 end
--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -4,51 +4,42 @@ describe OrderAddress do
   before do
     @order_address = FactoryBot.build(:order_address)
   end
-  describe '商品購入' do
-    context '購入がうまくいくとき' do
-      it "全て正しく入力されていれば購入可能" do
-      end
-      it "カード番号が入力されていれば購入可能" do
-      end
-      it "カードの有効期限が切れていなければ購入可能" do
-      end
-      it "cvcが正しく入力されていれば購入可能" do
-      end
-      it "郵便番号がハイフン込みで入力されていれば購入可能" do
-      end
-      it "都道府県が選択されていれば購入可能" do
-      end
-      it "市町村区が入力されていれば購入可能" do
-      end
-      it "電話番号がハイフンなしの数字のみで入力されていれば購入可能" do
-      end
-    end
-
-    context '購入がうまくいかないとき' do
-      it "カード番号が空だと購入できない" do
-      end
-      it "カード番号が正しくないと購入できない" do
-      end
-      it "cvcが空だと購入できない" do
-      end
-      it "cvcが正しくないと購入できない" do
-      end
-      it "カードの有効期限が空だと購入できない" do
-      end
-      it "カードの有効期限が切れていると購入できない" do
-      end
-      it "郵便番号が空だと購入できない" do
-      end
-      it "郵便番号にハイフンが含まれていないと購入できない" do
-      end
-      it "都道府県が選択されていないと購入できない" do
-      end
-      it "市町村区が空だと購入できない" do
-      end
-      it "電話番号が空だと購入できない" do
-      end
-      it "電話番号にハイフンが含まれていると購入できない" do
-      end
-    end
+  it "全て正しく入力されていれば購入可能" do
+     expect(@order_address).to be_valid
+  end
+  it "カード番号が空だと購入できない" do
+    @order_address.token = nil
+    @order_address.valid?
+    expect(@order_address.errors.full_messages).to include("Token can't be blank")
+  end
+  it "郵便番号が空だと購入できない" do
+    @order_address.postal_code = nil
+    @order_address.valid?
+    expect(@order_address.errors.full_messages).to include("Postal code can't be blank")
+  end
+  it "郵便番号にハイフンが含まれていないと購入できない" do
+    @order_address.postal_code = Faker::Address.postcode.delete("-")
+    @order_address.valid?
+    expect(@order_address.errors.full_messages).to include("Postal code is invalid. Include hyphen(-)")
+  end
+  it "都道府県が選択されていないと購入できない" do
+    @order_address.prefecture = 1
+    @order_address.valid?
+    expect(@order_address.errors.full_messages).to include("Prefecture can't be blank")
+  end
+  it "市町村区が空だと購入できない" do
+    @order_address.city = nil
+    @order_address.valid?
+    expect(@order_address.errors.full_messages).to include("City can't be blank")
+  end
+  it "電話番号が空だと購入できない" do
+    @order_address.phone_number = nil
+    @order_address.valid?
+    expect(@order_address.errors.full_messages).to include("Phone number can't be blank")
+  end
+  it "電話番号にハイフンが含まれていると購入できない" do
+    @order_address.phone_number = Faker::PhoneNumber.cell_phone
+    @order_address.valid?
+    expect(@order_address.errors.full_messages).to include("Phone number is invalid. You can't use hyphen(-)")
   end
 end
