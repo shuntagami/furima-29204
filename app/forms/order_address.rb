86e1d6399548dd68abc1c218fc26450a53305f1d
shuntagami <shuntagami23@gmail.com>
2020-09-11 14:12:38 +0900

    tokenのvalidation位置修正

--- app/forms/order_address.rb
+++ app/forms/order_address.rb
@@ -1,9 +1,8 @@
 class OrderAddress
   include ActiveModel::Model
   attr_accessor :user_id, :item_id, :order_id, :postal_code, :prefecture, :city, :banti, :building_name, :phone_number, :token
-  validates :token, presence: true
   with_options presence: true do
-    validates :city, :banti
+    validates :city, :banti, :token
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :phone_number, format: {with: /\A[0-9]+\z/, message: "is invalid. You can't use hyphen(-)"}
     validates :prefecture, numericality: { other_than: 1, message: "can't be blank" }
