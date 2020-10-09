b2e8736420ecb0dddc697bd4dde88cabf7ab79ca
shuntagami <shuntagami23@gmail.com>
2020-10-09 14:56:14 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :move_to_index, except: [:index, :show]
-  before_action :find_item, only: [:show, :edit, :update,]
+  before_action :find_item, only: [:show, :edit, :update]
   def new
     @item = Item.new
   end
